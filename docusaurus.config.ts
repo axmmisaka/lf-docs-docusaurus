e1e270f22bc30782b57e6765d4bc44d651d9e5ba
axmmisaka <6500159+axmmisaka@users.noreply.github.com>
2023-12-11 18:55:25 -0800

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
   url: 'https://www.lf-lang.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/lf-docs-docusaurus/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
